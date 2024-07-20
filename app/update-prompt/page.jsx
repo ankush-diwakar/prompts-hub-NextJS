d30d2a592b90e3d6f68d332ab860d8fde829a07e
ankush-diwakar <150348342+ankush-diwakar@users.noreply.github.com>
2024-07-20 19:20:31 +0530

    modified the next.config.js file

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -117,15 +117,15 @@ const UpdatePrompt = () => {
     };
 
     return (
-        <Suspense>
-            <Form
-                type='Edit'
-                post={post}
-                setPost={setPost}
-                submitting={submitting}
-                handleSubmit={updatePrompt}
-            />
-        </Suspense>
+
+        <Form
+            type='Edit'
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={updatePrompt}
+        />
+
     );
 };
 
