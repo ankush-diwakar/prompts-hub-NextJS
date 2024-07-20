037d2385816fa12e8490234f91c468f91d3a8f7b
ankush-diwakar <150348342+ankush-diwakar@users.noreply.github.com>
2024-07-20 21:39:27 +0530

    modified landing page

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -41,17 +41,6 @@ export const PATCH = async (request, { params }) => {
     }
 };
 
-// export const DELETE = async (request, { params }) => {
-//     try {
-//         await connectToDatabase();
-//         // Find the prompt by ID and remove it
-//         await Prompt.findByIdAndRemove(params.id);
-
-//         return new Response("Prompt deleted successfully", { status: 200 });
-//     } catch (error) {
-//         return new Response("Error deleting prompt", { status: 500 });
-//     }
-// };
 export const DELETE = async (request, { params }) => {
     try {
         await connectToDatabase();
